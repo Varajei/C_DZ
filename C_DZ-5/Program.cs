b36a33ab24679f2a7b70587458450fcde665de8e
Varajei <alibovie@mail.ru>
2023-04-07 15:27:26 +0300

    начата работа с ДЗ5

--- C_DZ-5/Program.cs
+++ C_DZ-5/Program.cs
@@ -4,6 +4,24 @@
 // [345, 897, 568, 234] -> 2
 
 
+int[] GetArray(int length, int minValue, int maxValue)
+{
+    int[] result = new int[length];
+    Random rand = new Random();
+    for (int i = 0; i < length; i++)
+        result[i] = rand.Next(minValue, maxValue + 1);
+    return result;
+}
+
+int[] NewArray(int[] array)
+{
+    
+}
+
+int[] array = GetArray(5, 100, 999); // 5 это количество элементов массива
+Console.Clear();
+Console.WriteLine(string.Join(", ", array));
+Console.WriteLine(string.Join(", ", NewArray(array)));
 
 
 // Задача 36: Задайте одномерный массив, заполненный случайными числами. Найдите сумму элементов, стоящих на нечётных позициях.
@@ -13,8 +31,38 @@
 // [-4, -6, 89, 6] -> 0
 
 
+// int[] GetArray(int length, int minValue, int maxValue)
+// {
+//     int[] result = new int[length];
+//     Random rand = new Random();
+//     for (int i = 0; i < length; i++)
+//         result[i] = rand.Next(minValue, maxValue + 1);
+//     return result;
+// }
 
+
+
+// int[] array = GetArray(5, 0, 10); // 5 это количество элементов массива
+// Console.Clear();
+// Console.WriteLine(string.Join(", ", array));
+// Console.WriteLine(string.Join(", ", NewArray(array)));
 
 // Задача 38: Задайте массив вещественных чисел. Найдите разницу между максимальным и минимальным элементов массива.
 
 // [3 7 22 2 78] -> 76
+
+// int[] GetArray(int length, int minValue, int maxValue)
+// {
+//     int[] result = new int[length];
+//     Random rand = new Random();
+//     for (int i = 0; i < length; i++)
+//         result[i] = rand.Next(minValue, maxValue + 1);
+//     return result;
+// }
+
+
+
+// int[] array = GetArray(5, 0, 10); // 5 это количество элементов массива
+// Console.Clear();
+// Console.WriteLine(string.Join(", ", array));
+// Console.WriteLine(string.Join(", ", NewArray(array)));
