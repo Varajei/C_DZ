db02338642a7beb7e5a3750690bde087f9f7b7fb
Varajei <alibovie@mail.ru>
2023-04-07 11:05:28 +0300

    Update Program.cs

--- C-DZ-4/Program.cs
+++ C-DZ-4/Program.cs
@@ -5,8 +5,35 @@
 
 // 2, 4 -> 16
 
+Console.Write("Введите первое число: ");
+string strNum1 = Console.ReadLine();
+int num1 = Convert.ToInt32(strNum1);
 
+Console.Write("Введите второе число: ");
+string strNum2 = Console.ReadLine();
+int num2 = Convert.ToInt32(strNum2);
 
+int GetArrey(int num1, int num2)
+{
+    int GetArrey = 1;
+    for (int i = 0; i < num2; i++)
+    {
+        GetArrey *= num1;
+    }
+    return GetArrey;
+}
+
+bool BooolArrey(int num2)
+{
+    if (num2 < 0)
+    {
+        Console.WriteLine("Больше нуля");
+        return false;
+    }
+    return true;
+}
+
+Console.WriteLine($"Число {num1} в степени {num2} равно {GetArrey(num1, num2)}");
 
 // Задача 27: Напишите программу, которая принимает на вход число и выдаёт сумму цифр в числе.
 
@@ -24,3 +51,4 @@
 // 1, 2, 5, 7, 19 -> [1, 2, 5, 7, 19]
 
 // 6, 1, 33 -> [6, 1, 33]
+
